f1ee7cc61d2ac44d87c0f368ecc201374622b3ba
Kraxpord <114495363+Kraxpord@users.noreply.github.com>
2022-10-10 00:50:51 +0300

    Add files via upload

---
+++ ht.m3.py
@@ -0,0 +1,7 @@
+numb = int(input('Input number - '))
+if numb>0:
+    print('Number is positive')
+elif numb<0:
+    print('Number is negative')
+elif numb==0:
+    print('Number is equal to zero')
---
+++ ht.m4.py
@@ -0,0 +1,8 @@
+numb1 = int(input('Введіть перше число - '))
+numb2 = int(input('Введіть друге число - '))
+if numb1==numb2:
+    print('Числа рівні')
+elif numb1 < numb2:
+    print(numb1,',',numb2)
+elif numb1 > numb2:
+    print(numb2,',',numb1)
