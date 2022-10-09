32882575e8ae27618ac400074920c3e43383f6f2
Kraxpord <114495363+Kraxpord@users.noreply.github.com>
2022-10-10 00:35:39 +0300

    Add files via upload

---
+++ ht.m.py
@@ -0,0 +1,17 @@
+day = int(input("Введіть день - "))
+if day == 1:
+    print('Понеділок')
+elif day == 2:
+    print('Вівторок')
+elif day == 3:
+    print('Середа')
+elif day == 4:
+    print('Четверг')
+elif day == 5:
+    print('Пятниця')
+elif day == 6:
+    print('Субота')
+elif day == 7:
+    print('Неділя')
+else:
+    print('Помилка!')
